7ea515798eec900e73af08b058661634fc443f09
Derek Garnett <rizzagum@gmail.com>
2024-11-01 17:55:05 +0000

    adds alo to stuff

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -3,6 +3,7 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useHistory } from "react-router";
+import { removeTokenTimestamp, shouldRefreshToken } from "../utils/utils";
 
 export const CurrentUserContext = createContext();
 export const SetCurrentUserContext = createContext();
@@ -30,6 +31,7 @@ export const CurrentUserProvider = ({ children }) => {
   useMemo(() => {
     axiosReq.interceptors.request.use(
       async (config) => {
+        if (shouldRefreshToken()) {
         try {
           await axios.post("/dj-rest-auth/token/refresh/");
         } catch (err) {
@@ -39,8 +41,10 @@ export const CurrentUserProvider = ({ children }) => {
             }
             return null;
           });
+          removeTokenTimestamp()
           return config;
         }
+      }
         return config;
       },
       (err) => {
@@ -61,6 +65,7 @@ export const CurrentUserProvider = ({ children }) => {
               }
               return null;
             });
+            removeTokenTimestamp();
           }
           return axios(err.config);
         }
